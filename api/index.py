24cae7749d9cb22e6973d2f64b7ca49a93703db0
katuoaikouka <yosinaka081@gmail.com>
2026-04-12 09:06:37 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -565,6 +565,7 @@ def high_quality_watch():
                            preferred_mode=preferred_mode)
 
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
